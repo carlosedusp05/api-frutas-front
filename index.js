885eee6e7492b8809a974700139524ee1e3adf97
CARLOS EDUARDO SILVA PEREIRA <25132872@CFP127ALUNO.LOCAL>
2025-10-06 16:59:13 -0300

    js iniciado

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const header = document.getElementById('cabecalho')
+
+const divHd = document.createElement('div')
+const inputHd = document.createElement('input')
+const buttonHd = document.createElement('button')
+
+const main = document.getElementById('principal')
+
+const divMnSug = document.createElement('div')
+const h1MnSug = document.createElement('h1')
+
+const containerMn = document.createElement('div')
+
+const boxMorangoMn = document.createElement('div')
+const buttonMorangoMn = document.createElement('button')
+const imgMorangoMn = document.createElement('img')
+const h2MorangoMn = document.createElement('h2')
+
+
+const boxBananaMn = document.createElement('div')
+const buttonBananaMn = document.createElement('button')
+const imgBananaMn = document.createElement('img')
+const h2BananaMn = document.createElement('h2')
+
+const boxLimaoMn = document.createElement('div')
+const buttonLimaoMn = document.createElement('button')
+const imgLimaoMn = document.createElement('img')
+const h2LimaoMn = document.createElement('h2')
+
+const boxAbacaxiMn = document.createElement('div')
+const buttonAbacaxiMn = document.createElement('button')
+const imgAbacaxiMn = document.createElement('img')
+const h2AbacaxiMn = document.createElement('h2')
+
+const boxMacaMn = document.createElement('div')
+const buttonMacaMn = document.createElement('button')
+const imgMacaMn = document.createElement('img')
+const h2MacaMn = document.createElement('h2')
+
+const boxBlueberryMn = document.createElement('div')
+const buttonBlueberryMn = document.createElement('button')
+const imgBlueberryMn = document.createElement('img')
+const h2BlueberryMn = document.createElement('h2')
+
+
+
+
+
+
