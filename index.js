208e08eab7fa937ed4dc673e040d1a199395bdee
CARLOS EDUARDO SILVA PEREIRA <25132872@CFP127ALUNO.LOCAL>
2025-10-14 17:06:54 -0300

    comecei o js

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 'use strict'
 
-const header = document.getElementById('cabecalho')
+const container = document.getElementById('container')
+const campo = document.getElementById('input')
+const imagemFruta = document.getElementById('fruta')
+const botaoBuscar = document.getElementById('buscar')
 
-const divHd = document.createElement('div')
-const inputHd = document.createElement('input')
-const buttonHd = document.createElement('button')
+async function buscarImagens (fruta) {
+    const url = `https://pixabay.com/api/?key=52528618-4626e693f82bda42876eaaacf&q=${fruta}&image_type=foto`
 
-const main = document.getElementById('principal')
+    const response = await fetch(url)
+    const imagens = await response.json()
 
-const divMnSug = document.createElement('div')
-const h1MnSug = document.createElement('h1')
+    console.log(imagens.message)
+    return imagens.message
+}
 
-const containerMn = document.createElement('div')
+function carregarImagens(urlDaImagem, fruta){
+    imagemFruta.textContent = ''
 
-const boxMorangoMn = document.createElement('div')
-const buttonMorangoMn = document.createElement('button')
-const imgMorangoMn = document.createElement('img')
-const h2MorangoMn = document.createElement('h2')
+    urlDaImagem.forEach(url => {
+        const nomeFruta = document.createElement('h1')
+        const imagem = document.createElement('img')
 
+        imagem.src = url
+        ligacao.href = url
+        ligacao.target = '_blank'
+        
+        imagemFruta.classList.add('fruta')
+        imagemFruta.appendChild(nomeFruta)
+        imagemFruta.appendChild(imagem)
 
-const boxBananaMn = document.createElement('div')
-const buttonBananaMn = document.createElement('button')
-const imgBananaMn = document.createElement('img')
-const h2BananaMn = document.createElement('h2')
+        nomeFruta = fruta
 
-const boxLimaoMn = document.createElement('div')
-const buttonLimaoMn = document.createElement('button')
-const imgLimaoMn = document.createElement('img')
-const h2LimaoMn = document.createElement('h2')
+    })
+}
 
-const boxAbacaxiMn = document.createElement('div')
-const buttonAbacaxiMn = document.createElement('button')
-const imgAbacaxiMn = document.createElement('img')
-const h2AbacaxiMn = document.createElement('h2')
+    let carregamento = async() => {
+        let pesquisa = campo.value
+    
+        let link = await buscarImagens(pesquisa)
+    
+        carregarImagens(link, pesquisa)
+    }
 
-const boxMacaMn = document.createElement('div')
-const buttonMacaMn = document.createElement('button')
-const imgMacaMn = document.createElement('img')
-const h2MacaMn = document.createElement('h2')
+function buscar (){
+    botaoBuscar.addEventListener('click', carregamento)
 
-const boxBlueberryMn = document.createElement('div')
-const buttonBlueberryMn = document.createElement('button')
-const imgBlueberryMn = document.createElement('img')
-const h2BlueberryMn = document.createElement('h2')
+    campo.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
+            carregamento()
+        }
+    })
+}
 
+buscar()
 
 
 
